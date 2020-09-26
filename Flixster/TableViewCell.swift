4818be6ebf850b81e5c9790592dc3c8415d37448
KennethChong <kennethchong39@gmail.com>
2020-09-26 10:52:35 -0700

    Testing

---
+++ Flixster/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Flixster
+//
+//  Created by Kenneth Chong on 9/26/20.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
