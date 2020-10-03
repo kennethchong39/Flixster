a539df31d9a87ab377b1806ebbe9fec397c0f378
KennethChong <kennethchong39@gmail.com>
2020-10-02 17:46:08 -0700

    Update more details about a particular movie and grid layout

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Kenneth Chong on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
